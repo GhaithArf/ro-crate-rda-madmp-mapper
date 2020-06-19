b8b1c77e1682cc81f033e627b55aa6644104114f
MarouaJaoua <maroua.jaoua@outlook.com>
2020-06-19 12:37:46 +0200

    add additional helper functions

--- utils.py
+++ utils.py
@@ -66,9 +66,9 @@ def flatten_json(dictionary):
     """Recursively flattens a nested json.
 
     :param dictionary: dict object to flatten
-    :return: OrderedDict containing flat json.
+    :return: dict object containing flat json.
     """
-    out = OrderedDict()
+    out = {}
 
     def flatten(element, name=''):
         if type(element) is dict:
@@ -82,7 +82,10 @@ def flatten_json(dictionary):
         else:
             out[name[:-1]] = element
     flatten(dictionary)
-    return out
+    out_ordered = OrderedDict()
+    for key, value in sorted(out.items()):
+        out_ordered[key] = value
+    return out_ordered
 
 
 def unflatten_json(dictionary):
@@ -125,6 +128,30 @@ def unflatten_json(dictionary):
     return resultDict
 
 
+def get_sequence_keys(directory):
+    """Recursively get all key sequences of a dict.
+
+    :param dictionary: dict object for which key sequences will be extracted
+    :return: list containing all key sequences.
+    """
+    keys_paths = []
+
+    def get_files(directory, prefix=[]):
+        if type(directory) is dict:
+            for filename in directory.keys():
+                path = prefix+[filename]
+                if isinstance(directory[filename], dict) or isinstance(directory[filename], list):
+                    get_files(directory[filename], path)
+                else:
+                    keys_paths.append(path)
+        elif type(directory) is list:
+            for i, element in zip(range(len(directory)), directory):
+                path = prefix+[i]
+                get_files(element, path)
+    get_files(directory)
+    return keys_paths
+
+
 def remove_digits_string(string):
     """Remove digits from a string and remove "." at the end of the string.
 
@@ -136,6 +163,60 @@ def remove_digits_string(string):
     clean_string = clean_string[:-
                                 1] if clean_string.endswith(".") else clean_string
     return clean_string
+
+def _add_key_value_all(dictonary, key="@id", value=""):
+    """Recursively add key ("@id") with value ("") to facilitate tracking.
+
+    :param dictonary: dict object
+    :param key: str of the key to add
+    :param value: str of the value to add
+    """
+    if type(dictonary) is dict:
+        for k, v in dictonary.items():
+            if type(v) is list:
+                _add_key_value_all(v, key, value)
+            elif type(v) is dict:
+                v[key] = value
+                _add_key_value_all(v, key, value)
+    elif type(dictonary) is list:
+        for item in dictonary:
+            if not type(item) is str:
+                item[key] = value
+                _add_key_value_all(item, key, value)
+
+def get_unnested_jsonld(dictionary):
+    """Unnest json to prepare for jsonld format.
+        "@id" should be present
+
+    :param dictionary: dict object to unnest
+    :return: dict object containing unnested json
+    """
+    keys_sequences = get_sequence_keys(dictionary)
+    for record in keys_sequences:
+        del record[-1]
+    keys_sequences = [list(i) for i in set(tuple(i)
+                                           for i in keys_sequences)]
+    keys_sequences.sort(key=len, reverse=True)
+
+    out = dictionary.copy()
+    for keys_sequence in keys_sequences:
+        if len(keys_sequence) >= 2:
+            val = out
+            for level in keys_sequence[:-1]:
+                val = val[level]
+            if type(val) is dict:
+                current_dict = val[keys_sequence[-1]]
+                current_dict["@type"] = keys_sequence[-1]
+            out.append(current_dict) if current_dict not in out else out
+            val2 = out
+            for level in keys_sequence[:-2]:
+                val2 = val2[level]
+            try:
+                val2[keys_sequence[-2]
+                     ] = {"@id": val2[keys_sequence[-2]][keys_sequence[-1]]["@id"]}
+            except:
+                print("Warning: @id not found")
+    return out
 
 
 def log(msg):
